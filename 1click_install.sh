75949a5daa64d7a126e83badbe5e461d36b6d5c5
maxwell <noemail>
2024-01-31 12:39:30 +0800

    Fix some bugs

--- 1click_install.sh
+++ 1click_install.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 set -e
-package1="git"
-package2="zsh"
 
 function ubuntu_config() {
-    # 使用dpkg -s来检查软件包是否已经安装
-    dpkg -s $package1 &> /dev/null
-
-    if [ $? -eq 0 ]; then
-        echo "$package1 is installed."
-    else
-        echo "input your password to install dependent packages"
+    if ! command -v git &> /dev/null
+    then
         # 1. 安装git
         sudo apt install git -y
+    else
+        echo "git is installed."
     fi
 
-    dpkg -s $package2 &> /dev/null
-
-    if [ $? -eq 0 ]; then
-        echo "$package2 is installed."
+    if grep -q "/bin/zsh" /etc/shells; then
+        echo "zsh is installed."
     else
         # 2. 安装zsh
         sudo apt install zsh -y
     fi
+
 }
 
 function centos_config() {
@@ -31,17 +25,15 @@ function centos_config() {
     # if语句就会执行then后面的代码块。
     if ! command -v git &> /dev/null
     then
-        echo "input your password to install dependent packages"
         # 1. 安装git
-        sudo yum install git -y
+        sudo yum install git-core -y
     else
-        echo "$package1 is installed."
+        echo "git is installed."
     fi
 
-
     # 判断zsh是否已经安装
-    if which zsh >/dev/null; then
-        echo "Zsh is installed."
+    if grep -q "/bin/zsh" /etc/shells; then
+        echo "zsh is installed."
     else
         # 2. 安装zsh
         echo "input your password to install dependent packages"
@@ -50,26 +42,26 @@ function centos_config() {
 }
 
 function mac_config() {
-    # 使用brew list来检查软件包是否已经安装
-    brew list $package1 &> /dev/null
-
-    if [ $? -eq 0 ]; then
-        echo "$package1 is installed."
-    else
-        echo "input your password to install dependent packages"
+    if ! command -v git &> /dev/null
+    then
         # 1. 安装git
         brew install git
+    else
+        echo "git is installed."
     fi
 
     # 判断zsh是否已经安装
-    if which zsh >/dev/null; then
-        echo "Zsh is installed."
+    if grep -q "/bin/zsh" /etc/shells; then
+        echo "zsh is installed."
     else
         # 2. 安装zsh
         brew install zsh
     fi
 
 }
+
+
+echo "you may need to input your password to install some dependent packages(git zsh)"
 
 # 使用uname -s获取操作系统名称
 os_name=$(uname -s)
@@ -100,6 +92,27 @@ fi
 # 进入用户目录
 cd
 
+# Git 可能无法验证清华镜像服务器的 SSL 证书。
+# 这并不意味着证书有问题，但可能是自签名的，或者由一个不在您的操作系统的 CA 列表中的机构/公司签名的。
+if  ! command -v sslverify=$(git config --global --get http.sslverify) 
+    then
+        if [ -z "$sslverify" ]; then
+            # 将其设置为false
+            git config --global http.sslverify false
+        else
+            if [ "$sslverify" == "true" ]; then
+            # 先关掉这个验证
+            git config --global http.sslverify false
+            fi
+        fi
+    fi
+sslverify=$(git config --global --get http.sslverify)
+
+
+sslverify_state=$sslverify
+
+
+
 # 下载oh-my-zsh安装脚本
 git clone https://mirrors.tuna.tsinghua.edu.cn/git/ohmyzsh.git
 
@@ -117,10 +130,18 @@ git clone https://mirror.nju.edu.cn/git/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-
 # 安装zsh-syntax-highlighting插件 使用南京大学的镜像
 git clone https://mirror.nju.edu.cn/git/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# 如果之前是true，就把它改回去
+if [ "$sslverify" == "true" ]; then 
+    git config --global http.sslverify true
+fi
+
 # 切换到用户的根目录
 cd
 # 修改.zshrc文件中的plugins配置
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' .zshrc
+
+# 清理安装文件
+rm -rf ~/ohmyzsh
 
 # 设置默认终端
 echo "input your password to set zsh as default shell"
--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # 1clickohmyzsh
 
-一条命令安装oh my zsh。再附加两个常用的插件：语法高亮和自动提示。
+一条命令安装oh my zsh。再附加两个常用的插件：语法高亮（zsh-syntax-highlighting）和自动提示（zsh-autosuggestions）。
 
 ## Warning
 
-该脚本仅支持在ubuntu/centos/mac上运行。
+该脚本仅支持在ubuntu/centos/mac上运行，**Windows无法使用**。
 
 如果您的系统基于 Debian/Redhat，您可以尝试运行此脚本，它很可能是可以用的。
 
 ## Dependency
 
-请确保 lsb_release 命令可以在您的计算机上运行。 如果没有，就想办法安装。
+请确保`grep`,  lsb_release 命令可以在您的计算机上运行。 如果没有，就想办法安装。
 
 请确保您具有 sudo 权限。 没有它就无法工作。
 
